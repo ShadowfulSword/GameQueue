5eebc12002efac1de9fb647aa3981480c4dbbc26
ShadowfulSword <151599199+ShadowfulSword@users.noreply.github.com>
2026-03-21 22:37:33 -0500

    Finished retrieve game data

--- backend/Steam/clients.py
+++ backend/Steam/clients.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+import time
 
 #load_dotenv()
 #apikey = os.getenv("STEAM_API_KEY")
@@ -22,7 +23,7 @@ def get_games(steamid, apikey):
     response = requests.get(url, params=params)
     try:
         response.raise_for_status()
-    except response.exception.HTTPError as e:
+    except requests.exceptions.HTTPError as e:
         print(f"HTTP error: {e}")
         return []
     data = response.json()
@@ -35,3 +36,23 @@ def get_games(steamid, apikey):
     return games
 
 #print(get_games(steamid, apikey))
+
+#get the full game details from a game -- wait 1.5 seconds so you dont blow up the 200 requests 
+#every 5 mins on steam api
+def get_game_details(appid):
+    time.sleep(1.5)
+    url= f"https://store.steampowered.com/api/appdetails?appids={appid}"
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error: {e}")
+        return None
+    data = response.json()
+    if(data[str(appid)]["success"]):
+        return data[str(appid)]["data"]
+    else:
+        return None
+
+    
+#print(get_game_details(570))
